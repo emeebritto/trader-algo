a479226c07161af9aade89636c849b06cc79d399
Emerson-Britto <emersonbritto987@gmail.com>
2022-07-16 02:29:30 -0300

    [removed]: pyautogui

--- utils/screen.py
+++ utils/screen.py
@@ -1,25 +1,25 @@
-import pyautogui as pg
+# import pyautogui as pg
 import os
 import pyscreeze
 
 class Screen:
 	def __init__(self):
 		self.hasDisplay = not bool(os.environ.get("NO_DISPLAY"))
-		if self.hasDisplay: self._getScreenSize()
+		# if self.hasDisplay: self._getScreenSize()
 
 
-	def _getScreenSize(self):
-		screenSize = pg.size()
-		self.width = screenSize.width
-		self.height = screenSize.height
+	# def _getScreenSize(self):
+	# 	screenSize = pg.size()
+	# 	self.width = screenSize.width
+	# 	self.height = screenSize.height
 	
 
-	def take_screenshot(self, region):
-		return pg.screenshot(region=region)
+	# def take_screenshot(self, region):
+	# 	return pg.screenshot(region=region)
 
 
-	def locateOnScreen(self, baseFile, confidence, region):
-		return pg.locateOnScreen(baseFile, confidence=confidence, region=region)
+	# def locateOnScreen(self, baseFile, confidence, region):
+	# 	return pg.locateOnScreen(baseFile, confidence=confidence, region=region)
 
 
 	def locateOnImage(self, image, needleImage, confidence):
